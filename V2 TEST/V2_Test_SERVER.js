216e37e210f1c4e5113104c2d60bd91f28c53d3d
wakaba0972 <benny.wu2199@gmail.com>
2021-12-16 02:18:41 +0800

    V2

--- V2 TEST/V2_Test_SERVER.js
+++ V2 TEST/V2_Test_SERVER.js
@@ -1,23 +1,61 @@
 var request = require("request")
 var express = require("express")
-var fs = require("fs")
+var cheerio = require("cheerio")
 
 var app = express()
+app.use(express.json())
+app.use('/public', express.static(__dirname + '\\public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '\\V2_Test_CLIENT.html')
 })
 
-app.get('/img', function(req, res){
-    console.log('hi')
-    var requestSettings = {
-        url: 'https://i.nhentai.net/galleries/1217339/1.jpg',
+app.post('/sendNumber', function(req, res){
+    const urlNumber = req.body['urlNumber']
+    console.log(Date.now() + ' ' + req.ip + ' : ' + urlNumber)
+
+    if(isNaN(parseInt(urlNumber))){
+        res.end('Please Enter Valid Number')
+        return
+    }
+
+    request('https://nhentai.net/g/' + urlNumber + '/', (err, res2, body) =>{
+        const $ = cheerio.load(body)
+        let pages = $('.name').last().text()
+
+        if($('h1').text() == '404 – Not Found'){
+            res.end('No Such Manga')
+        }
+        else if(pages > 100){
+            res.end('Too Many Pages!')
+        }
+        else{
+            let imgElems = $('.lazyload')
+            let imgUrls = imgElems.get().map(x => $(x).attr('data-src'))
+            let galleryNumber =imgUrls[0].split('/')[4]
+
+            var response = {}
+            response['pages'] = pages
+            response['galleryNumber'] = galleryNumber
+            for(let i=1; i<=pages; ++i){
+                response[i] = imgUrls[i].substr(-3)
+            }
+            res.json(response)
+        }
+    })
+})
+
+app.get('/image', (req, res) => {
+    const requestSettings = {
+        url: 'https://i.nhentai.net/galleries/' + req.query.parm,
         method: 'GET',
         encoding: 'base64'
     };
     request(requestSettings, (err, res2, data) => {
-        res.end(data);
+        res.type('jpeg')
+        res.end(data)
     })
+
 })
 
 
